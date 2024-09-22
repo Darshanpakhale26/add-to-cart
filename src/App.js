2780fa1c5baf957f32c70684cb073b98ef31d5b0
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2024-09-22 13:43:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import Navbar from './components/NavBar'
+import Shop from './components/Shop'
+import './App.css';
+import Cart from './components/Cart';
+
+const App = () => {
+
+  const [cart,setCart] = useState([]);     // cart is an array of objects
+  const [warning,setWarning] = useState(false);     // warning is a boolean
+  const[show,setShow] = useState(true);        // show is a boolean
+
+  const handleClick=(item) =>{
+    let isPresent = false;
+    cart.forEach((product) =>{
+      if(item.id === product.id)
+      isPresent= true;                    // checking if item is already present in cart
+    })
+
+    if(isPresent){
+      setWarning(true);
+      setTimeout(()=>{
+        setWarning(false);
+      },2000);
+      return;
+    }                                   // if item is already present in cart then it will show warning
+    setCart([...cart,item]);
+  }
+
+  const handleChange = (item,d) => {
+    let ind = -1;
+    cart.forEach((data,index) => {
+      if(data.id === item.id)         // checking the index of item in cart
+      ind = index;
+    });
+    const tempArr = cart;
+    tempArr[ind].amount +=d;
+    console.log(tempArr);
+
+    if(tempArr[ind].amount === 0){
+      tempArr[ind].amount =1;
+      
+    }
+    setCart([...tempArr])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar size={cart.length} setShow={setShow}/>
+      {
+        show ? <Shop handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange}/>
+      }
+      
+      {
+        warning && <div className='warning'> Item is already in your cart </div>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
